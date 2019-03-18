8e5f1537972cb80ee101abee18efb0977fa12545
Michael Schiumo <mschiumo18@gmail.com>
2019-03-18 16:00:08 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,7 @@
 class Student < ActiveRecord::Base
 
-  def concatenate
-    @name = "#{first_name}" + "#{last_name}"
-    @name.to_s
+  def to_s
+    self.first_name + " " + self.last_name
   end
 
 end
